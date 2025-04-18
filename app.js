07d903d4435b0a94965b9792c978620e6e83cdc4
lerryberry <eschapple@gmail.com>
2025-04-18 11:28:30 +1000

    package.json changes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-//middleware
 const express = require('express');
-//tracing
 const morgan = require('morgan');
 const { auth, requiresAuth } = require('express-openid-connect');
-
-//TODO might not need long term, using only in auth setup / testing
-const path = require('path');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongosanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 //import controllers
 const AppError = require('./utils/appError');
@@ -15,11 +17,37 @@ const decisionRouter = require('./routes/decisionRoutes');
 //middleware to start express
 const app = express();
 
-//if dev env, make logging verbose
+//security HTTP headers
+app.use(helmet());
+
+//global middlewares.. if dev env, make logging verbose
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
     console.log("Morgan in development mode")
 }
+
+//rate limiter
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000, // 1 hour
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use(limiter);
+
+//security, sanitisation and other middlewares
+app.use(express.json({ limit: '10kb' })); // limit to 10kb
+app.use(mongosanitize()); //sanitize data against NoSQL query injection
+app.use(xss()); //sanitize data against XSS attacks
+app.use(hpp({ //prevent parameter pollution
+    whitelist: ['duration', 'ratingsAverage', 'ratingsQuantity']
+}));
+app.use(cookieParser()); //parse cookies
+app.use(compression()); //compress response bodies for all requests
+
+//serving static files
+app.use(express.static(`${__dirname}/public`)); // static files in public folder
+//parse urlencoded data
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // limit to 10kb
 
 app.use(express.json());
 
@@ -37,27 +65,20 @@ const oidcConfig = {
 app.use(auth(oidcConfig));
 
 //TODO might not need long term, using only in auth setup
+const path = require('path');
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html')); // Serve about.html
 });
-
-//TODO might not need long term, using only in auth setup
-// req.isAuthenticated is provided from the auth router
 app.get('/loginstatus', (req, res) => {
     res.send(req.oidc.isAuthenticated());
 });
 
 //applies to all subsequent routes
 app.use(requiresAuth());
-
-//api routes
-//app.use('/api/v1/criteria', criteriaRouter);
 app.use('/api/v1/decisions', decisionRouter);
 
 //TODO might not need long term, using only in auth setup
 app.get('/details', (req, res) => {
-    const x = req.oidc.user
-    console.log(x)
     res.send(req.oidc.user);
 });
 
