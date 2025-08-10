1521dbd8dfb64f14c7e14f6cdcce8d98a17c8293
lerryberry <eschapple@gmail.com>
2025-08-10 15:45:41 +1000

    merp

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -25,7 +25,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
             clientId="sZ2ckim3v3suG8XLZCPoF24ueQCIl3co"
             authorizationParams={{
                 redirect_uri: window.location.origin,
-                audience: 'https://localhost:3000',
+                audience: 'http://localhost:3000',
                 scope: 'openid profile email'
             }}
             cacheLocation="localstorage"
