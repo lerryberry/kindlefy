7732eee912832440085da83ebc50ee7d94e16730
lerryberry <eschapple@gmail.com>
2025-08-23 14:39:23 +1000

    Fix Heroku TypeScript build errors

---
+++ frontend/src/types/react.d.ts
@@ -0,0 +1,15 @@
+declare module 'react/jsx-runtime' {
+    export default any;
+    export const jsx: any;
+    export const jsxs: any;
+    export const Fragment: any;
+}
+
+declare module 'react' {
+    export = React;
+    export as namespace React;
+}
+
+declare module 'react-dom/client' {
+    export * from 'react-dom/client';
+}
